970cf7534d9369b243597c0984dab270e9fc1c1f
Yunseong Jung <davidjung171@gmail.com>
2023-11-24 07:50:05 +0900

    commit

---
+++ server/server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const stripe = require("stripe");
+
+dotenv.config();
+const app = express();
+
+app.use(express.static("views"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: "views" });
+});
+
+app.get("/success", (req, res) => {
+    res.sendFile("success.html", { root: "views" });
+});
+
+app.get("/failure", (req, res) => {
+    res.sendFile("failure.html", { root: "views" });
+});
+
+let stripeGateway = stripe(process.env.stripe_api);
+let DOMAIN = process.env.DOMAIN;
+
+app.post("/stripe-checkout", async (req, res) => {
+    const lineItems = req.body.items.map((item) => {
+        const unitAmount = parseInt(item.price.replace(/[^0-9.-]+/g, "") * 100);
+        console.log("item-price:", item.price);
+        console.log("unitAmount:", unitAmount);
+        return {
+            price_data:{
+                currency:"usd",
+                product_data: {
+                    name:item.title,
+                    images: [item.productImg]
+                },
+                unit_amount: unitAmount, 
+            },
+            quantity: item.quantity,
+        }
+    });
+    console.log("lineItems:", lineItems);
+    const session = await stripeGateway.checkout.sessions.create({
+        payment_method_types: ["card"], 
+        mode: "payment",
+        success_url: `${DOMAIN}/success`,
+        cancel_url: `${DOMAIN}/failure`,
+        line_items: lineItems,
+        billing_address_collection: "required",
+    });
+    res.json(session.url);
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
